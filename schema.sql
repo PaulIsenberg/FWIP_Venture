7e415e0ca7227b83c08943b80fc83e83b3b2ee38
winston.shih88 <winston.shih88@gmail.com>
2020-08-04 13:05:32 -0700

    updated schema and completed pre-processing for SQL integration

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 CREATE TABLE objects(
+	index INT NOT NULL,
 	object_id VARCHAR NOT NULL,
 	entity_type VARCHAR NOT NULL,
 	parent_id VARCHAR,
@@ -28,31 +29,34 @@ UNIQUE (object_id)
 );	
 	
 CREATE TABLE offices(
+	index INT NOT NULL,
 	object_id VARCHAR NOT NULL,
-	description VARCHAR,
 	region VARCHAR,
 	address1 VARCHAR,
 	address2 VARCHAR,
 	city VARCHAR,
-	zip_code INT,
+	zip_code VARCHAR,
 	state_code VARCHAR,
 	country_code VARCHAR,
-	latitude FLOAT,
-	longitude FLOAT,
+	latitude VARCHAR,
+	longitude VARCHAR,
 FOREIGN KEY (object_id) REFERENCES objects(object_id)
 );
 
 CREATE TABLE ipos(
+	index INT NOT NULL,
 	object_id VARCHAR NOT NULL,
 	valuation_amount FLOAT,
 	valuation_currency_code VARCHAR,
 	raised_amount FLOAT,
+	raised_currency_code VARCHAR,
 	public_at DATE,
 	stock_symbol VARCHAR,
 FOREIGN KEY (object_id) REFERENCES objects(object_id)
 );
 
 CREATE TABLE milestones(
+	index INT NOT NULL,
 	object_id VARCHAR NOT NULL,
 	milestone_at DATE,
 	milestone_code VARCHAR,
@@ -60,6 +64,7 @@ FOREIGN KEY (object_id) REFERENCES objects(object_id)
 );
 
 CREATE TABLE people(
+	index INT NOT NULL,
 	object_id VARCHAR NOT NULL,
 	first_name VARCHAR,
 	last_name VARCHAR,
@@ -68,6 +73,7 @@ FOREIGN KEY (object_id) REFERENCES objects(object_id)
 );
 
 CREATE TABLE degrees(
+	index INT NOT NULL,
 	object_id VARCHAR NOT NULL,
 	degree_type VARCHAR,
 	subject VARCHAR,
@@ -77,6 +83,7 @@ FOREIGN KEY (object_id) REFERENCES objects(object_id)
 );
 
 CREATE TABLE funding_rounds(
+	index INT NOT NULL,
 	funding_round_id INT NOT NULL,
 	object_id VARCHAR NOT NULL,
 	funded_at DATE,
@@ -89,6 +96,7 @@ CREATE TABLE funding_rounds(
 	pre_money_valuation FLOAT,
 	pre_money_currency_code VARCHAR,
 	post_money_valuation_usd FLOAT,
+	post_money_valuation FLOAT,
 	post_money_currency_code VARCHAR,
 	participants INT,
 	is_first_round BOOLEAN,
@@ -97,6 +105,7 @@ FOREIGN KEY (object_id) REFERENCES objects(object_id)
 );
 
 CREATE TABLE funds(
+	index INT NOT NULL,
 	object_id VARCHAR NOT NULL,
 	name VARCHAR NOT NULL,
 	funded_at DATE,
@@ -106,31 +115,35 @@ FOREIGN KEY (object_id) REFERENCES objects(object_id)
 );
 
 CREATE TABLE relationships(
+	index INT NOT NULL,
 	person_object_id VARCHAR NOT NULL,
 	relationship_object_id VARCHAR,
 	start_at DATE,
 	end_at DATE,
-	is_past DATE,
+	is_past BOOLEAN,
 	sequence INT,
 	title VARCHAR,
 FOREIGN KEY (person_object_id) REFERENCES objects(object_id)
 );
 
 CREATE TABLE investments(
+	index INT NOT NULL,
 	funding_round_id INT NOT NULL,
 	funded_object_id VARCHAR NOT NULL,
-	investor_object_id VARCAR NOT NULL,
+	investor_object_id VARCHAR NOT NULL,
 FOREIGN KEY (funded_object_id) REFERENCES objects(object_id),
 FOREIGN KEY (investor_object_id) REFERENCES objects(object_id)
 );
 
 CREATE TABLE acquisitions(
-	acquiring_object_id VARCHAR NOT NULL,
-	acquired_object_id  VARCHAR NOT NULL,
+	index INT NOT NULL,
+	object_id VARCHAR,
+	object_id  VARCHAR,
 	term_code VARCHAR,
 	price_amount FLOAT,
 	price_currency_code VARCHAR,
 	acquired_at DATE,
-FOREIGN KEY (acquiring_object_id) REFERENCES objects(object_id),
-FOREIGN KEY (acquired_object_id) REFERENCES objects(object_id)
+FOREIGN KEY (object_id) REFERENCES objects(object_id),
+FOREIGN KEY (object_id) REFERENCES objects(object_id)
 );
+
